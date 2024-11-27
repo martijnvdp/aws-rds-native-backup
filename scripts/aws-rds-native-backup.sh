4920626e8cfe1d8bf14e7285a5e4b3a6a26fcfab
martijnvdp <m.vanderploeg@nl.ccv.eu>
2024-11-27 16:16:29 +0100

    init

--- scripts/aws-rds-native-backup.sh
+++ scripts/aws-rds-native-backup.sh
@@ -37,6 +37,8 @@ fi
 echo "Printing Volume Information"
 df -h .
 
+RDS_PORT="3306"
+
 # get backup targets from rds cluster tags
 clusters=$(aws rds describe-db-clusters)
 clusterIdentifiers=$(echo $clusters|yq '.DBClusters[].DBClusterIdentifier')
@@ -48,21 +50,24 @@ do
         RDS_HOST=$(echo $clusters| yq '.DBClusters[] | select(.DBClusterIdentifier == "'${cluster}'")'| yq .ReaderEndpoint)
         RDS_ENGINE=$(echo $clusters| yq '.DBClusters[] | select(.DBClusterIdentifier == "'${cluster}'")'| yq .Engine)
         
+        if [ $RDS_ENGINE == "aurora-postgresql"]; then
+            RDS_PORT="5432"
+        fi
+
         echo "get iam token for user ${RDS_USER} ..."
         export PGPASSWORD=$(aws rds generate-db-auth-token --hostname ${RDS_HOST} --port ${RDS_PORT} --username ${RDS_USER} --region ${REGION})
         
-        Databases=$( psql --username=$MYSQLUser  -h $RDSHost -c "select datname from pg_database" -d postgres --csv)
+        Databases=$( psql --username=$RDS_USER  -h $RDS_HOST -c "select datname from pg_database" -d postgres --csv)
 
-        for RSS_DATABASE in $Databases
+        for RDS_DATABASE in $Databases
         do
-            FILENAME=${DATABASE}_$(date +"%Y-%m-%dT%H:%M:%SZ")
+            FILENAME=${RDS_DATABASE}_$(date +"%Y-%m-%dT%H:%M:%SZ")
             echo "Using Filename: ${FILENAME}"
 
             if [ $RDS_ENGINE == "aurora-postgresql"]; then
-                RDS_PORT="5432"
                 echo "Start dump.."
-                echo "pg_dump -h "${RDS_HOST}" -p "${RDS_PORT}" -U "${RDS_USER}" -bCc -d "${DATABASE}" sslmode=verify-full sslrootcert=eu-west-1-bundle.pem" 
-                # { pg_dump -h "${RDS_HOST}" -p "${RDS_PORT}" -U "${RDS_USER}" -bCc -d "${DATABASE}" sslmode=verify-full sslrootcert=eu-west-1-bundle.pem |\
+                echo "pg_dump -h "${RDS_HOST}" -p "${RDS_PORT}" -U "${RDS_USER}" -bCc -d "${RDS_DATABASE}" sslmode=verify-full sslrootcert=eu-west-1-bundle.pem" 
+                # { pg_dump -h "${RDS_HOST}" -p "${RDS_PORT}" -U "${RDS_USER}" -bCc -d "${RDS_DATABASE}" sslmode=verify-full sslrootcert=eu-west-1-bundle.pem |\
                 # bzip2 |\
                 # aws s3 cp - "s3://$S3_BUCKET/$S3_PREFIX/${FILENAME}.sql.bz" --region=$S3_REGION; } 3>&1 | tr '\015' '\012'
                 echo "Done."
