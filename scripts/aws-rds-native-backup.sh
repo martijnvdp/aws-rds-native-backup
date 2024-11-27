c7a1cee612f2245dfdf6c827d822a8770a4d6688
martijnvdp <m.vanderploeg@nl.ccv.eu>
2024-11-27 15:36:40 +0100

    init

--- scripts/aws-rds-native-backup.sh
+++ scripts/aws-rds-native-backup.sh
@@ -4,24 +4,29 @@ set -euo pipefail
 
 # Check environment
 
+if [ -z "${BACKUP_TAG_KEY:-}" ]; then
+  echo "BACKUP_TAG_KEY was not set"
+fi
+
+
 if [ -z "${REGION:-}" ]; then
   echo "REGION was not set"
 fi
 
-if [ -z "${POSTGRES_DATABASE:-}" ]; then
-  echo "POSTGRES_DATABASE was not set"
+if [ -z "${RDS_DATABASE:-}" ]; then
+  echo "RDS_DATABASE was not set"
 fi
 
-if [ -z "${POSTGRES_HOST:-}" ]; then
-  echo "POSTGRES_HOST was not set"
+if [ -z "${RDS_HOST:-}" ]; then
+  echo "RDS_HOST was not set"
 fi
 
-if [ -z "${POSTGRES_PORT:-}" ]; then
-  echo "POSTGRES_HOST was not set"
+if [ -z "${RDS_PORT:-}" ]; then
+  echo "RDS_HOST was not set"
 fi
 
-if [ -z "${POSTGRES_USER:-}" ]; then
-  echo "POSTGRES_HOST was not set"
+if [ -z "${RDS_USER:-}" ]; then
+  echo "RDS_HOST was not set"
 fi
 
 if [ -z "${S3_BUCKET:-}" ]; then
@@ -44,14 +49,38 @@ fi
 echo "Printing Volume Information"
 df -h .
 
-FILENAME=${POSTGRES_DATABASE}_$(date +"%Y-%m-%dT%H:%M:%SZ")
-echo "Using Filename: ${FILENAME}"
+# get backup targets from rds cluster tags
+clusters=$(aws rds describe-db-clusters)
+clusterIdentifiers=$(echo $clusters|yq '.DBClusters[].DBClusterIdentifier')
+for cluster in $clusterIdentifiers
+do
+    echo "Getting tags from RDS cluster ${cluster}.."
+    if [ "$(echo $clusters| yq '.DBClusters[] | select(.DBClusterIdentifier == "'${cluster}'")'| yq .TagList| yq '.[] | select(.Key=="'${BACKUP_TAG_KEY}'")|.Value')" == true ]; then
+        echo "${cluster} has ${BACKUP_TAG_KEY} tag set to true"
+        RDS_HOST = $(echo $clusters| yq '.DBClusters[] | select(.DBClusterIdentifier == "'${cluster}'")'| yq .ReaderEndpoint)
+        RDS_ENGINE = $(echo $clusters| yq '.DBClusters[] | select(.DBClusterIdentifier == "'${cluster}'")'| yq .Engine)
+        
+        echo "get iam token for user ${RDS_USER} ..."
+        export PGPASSWORD=$(aws rds generate-db-auth-token --hostname ${RDS_HOST} --port ${RDS_PORT} --username ${RDS_USER} --region ${REGION})
+        
+        Databases = $( psql --username=$MYSQLUser  -h $RDSHost -c "select datname from pg_database" -d postgres --csv)
 
-export PGPASSWORD=$(aws rds generate-db-auth-token --hostname ${POSTGRES_HOST} --port ${POSTGRES_PORT} --username ${POSTGRES_USER} --region ${REGION})
+        for RSS_DATABASE in $Databases
+        do
+            FILENAME=${DATABASE}_$(date +"%Y-%m-%dT%H:%M:%SZ")
+            echo "Using Filename: ${FILENAME}"
 
-echo "Start dump.."
-{ pg_dump -h "${POSTGRES_HOST}" -p "${POSTGRES_PORT}" -U ${POSTGRES_USER} "dbname=${POSTGRES_DATABASE} sslmode=verify-full sslrootcert=eu-west-1-bundle.pem" |\
-pv -L ${RATE_LIMIT} -r -b -i 60 -f 2>&3 |\
-bzip2 |\
-aws s3 cp - "s3://$S3_BUCKET/$S3_PREFIX/${FILENAME}.sql.bz.enc" --region=$S3_REGION; } 3>&1 | tr '\015' '\012'
-echo "Done."
+            if [ $RDS_ENGINE == "aurora-postgresql"]; then
+                RDS_PORT = "5432"
+                echo "Start dump.."
+                # { pg_dump -h "${RDS_HOST}" -p "${RDS_PORT}" -U "${RDS_USER}" -bCc -d "${DATABASE}" sslmode=verify-full sslrootcert=eu-west-1-bundle.pem |\
+                # bzip2 |\
+                # aws s3 cp - "s3://$S3_BUCKET/$S3_PREFIX/${FILENAME}.sql.bz" --region=$S3_REGION; } 3>&1 | tr '\015' '\012'
+                echo "Done."
+            fi
+        done
+    fi
+done
+
+
+
